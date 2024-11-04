834e28aed220d203f8e84dc37d4c0195569abcba
Joey <xiaxing.zhu16@gmail.com>
2024-11-04 07:39:17 -0800

    added error handling and download to directory in insta reels downloader

--- insta_reels_downloader.py
+++ insta_reels_downloader.py
@@ -1,18 +1,26 @@
 import instaloader
 
-def download_reels(url):
+def download_reels(url, target_dir = 'downloaded_reels'):
     url = url.rstrip('/')
     loader = instaloader.Instaloader()
     shortcode = url.split('/')[-1]
 
     try:
         post = instaloader.Post.from_shortcode(loader.context, shortcode)
-        loader.download_post(post, target='downloaded_reels')
+
+        loader.dirname_pattern = target_dir
+        loader.download_post(post, target_dir)
         print('Download successful!')
     except Exception as e:
-        print('Error: could not download...')
+        print(f'Error: could not download... Reasons: {e}')
 
-with open('link.txt') as reels:
+#set to your own custom file directory in pc
+repost_dir = r'C:\Users\dadax\OneDrive\Desktop\Instagram repost'
+
+with open('link.txt', 'r') as reels:
     for reel in reels:
-        download_reels(reel)
-    print('Download Complete.')
+        reel = reel.strip()
+
+        download_reels(reel, target_dir = repost_dir)
+
+    print('Download Complete.') 
